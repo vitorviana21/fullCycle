de1670dc60d0caa56a1bea891e1a95628da18c90
vitorviana21 <virtoviana18@gmail.com>
2025-04-09 22:37:16 -0300

    Aula dia 09/04

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// filepath: postcss.config.mjs
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
