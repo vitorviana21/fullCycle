baa456766c10dbc178bd15ada553bc441c108b6a
vitorviana21 <virtoviana18@gmail.com>
2025-04-10 22:07:01 -0300

    Aula dia 10/04

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,42 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <div className ='relative h-full  bg-black lg:h-[140vh]' >
-    <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'></main>
+   <div className ="relative h-screen overflow-hidden bg-gradient-to-b from-transparent via-black to-black lg:h-[140vh]" >
+    {/* <Header /> }*/}
+    <main className='relative pb-24 pl-4  lg:pl-16'>
+      <div className= 'flex flex-col  space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
+        <div className= 'absolute  flex flex-col left-0 top-0 -z-10 h-[95vh] w-screen bg-black'>
+        <Image
+          src='/banner.jpg'
+          alt='The Whitcher'
+          fill= {true}
+          className= 'h-[65vh] object-cover object-top lg:h-[95vh] '
+          />
+        </div>
+ 
+        
+        
+          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
+            MAID
+          </h1>
+
+          <p className= 'text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
+            The Maid is a 2021 American drama television miniseries based on the
+            memoir of the same name by Stephanie Land. It stars Margaret Qualley
+            as a single mother who becomes a maid to make ends meet while
+            struggling with poverty and her abusive relationship with her
+            daughter's father, played by Nick Robinson.
+          </p>
+       
+      </div>
+
+      <div className='flex space-x-3'>
+        <button className= 'md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>Play</button>
+        <button className= 'md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>More Info</button>
+      </div>
+
+
+    </main>
   </div>
   );
 }
