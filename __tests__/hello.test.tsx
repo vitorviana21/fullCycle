6eba4344102db742f18cee62d36e10af0d879adb
vitorviana21 <virtoviana18@gmail.com>
2025-04-21 20:36:03 -0300

    Aula dia 21/04 v2

---
+++ __tests__/hello.test.tsx
@@ -0,0 +1,9 @@
+describe('Hello World Tests', () => {
+    it('should return true for true', () => {
+        expect(true).toBe(true);
+    });
+
+    it('should add numbers correctly', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
